e1e9e05b133c924d19b178d728ca2e6f93ff1dda
mfmarano <mmarano@frba.utn.edu.ar>
2024-09-22 19:54:35 -0300

    test: aserciones

--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,17 +1,36 @@
 require_relative 'spec_helper'
 
-class Prueba
-  def materia
-    :tadp
-  end
-end
+describe 'TADsPec' do
 
-describe 'Prueba' do
-  let(:prueba) { Prueba.new }
+  describe 'Aserciones' do
 
-  describe '#materia' do
-    it 'debería pasar este test' do
-      expect(prueba.materia).to be :tadp
+    self.class_eval do
+      include Aserciones
     end
+
+    it 'true es igual a true' do
+      true.deberia ser true
+    end
+
+    it '7 es igual a 7' do
+      7.deberia ser 7
+    end
+
+    it '30 es mayor a 20' do
+      30.deberia ser mayor_a 20
+    end
+
+    it '20 es menor que 25' do
+      20.deberia ser menor_a 25
+    end
+
+    it '7 esta dentro de la lista' do
+      7.deberia ser uno_de_estos [7, 22, "hola"]
+    end
+
+    it 'hola esta dentro de la lista' do
+      "hola".deberia ser uno_de_estos 7, 22, "hola"
+    end
+
   end
 end
