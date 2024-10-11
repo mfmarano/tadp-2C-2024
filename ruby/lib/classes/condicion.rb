a07b696ffdb3396b4e5779752caa909a41b3d807
mfmarano <mmarano@frba.utn.edu.ar>
2024-10-11 11:16:20 -0300

    refactor: raise excepcion en Condicion [WIP]

--- ruby/lib/classes/condicion.rb
+++ ruby/lib/classes/condicion.rb
@@ -3,7 +3,13 @@ class Condicion
     @proposicion = proposicion
   end
 
+  def self.crear_condicion(mensaje, &proposicion)
+    condicion = Condicion.new &proposicion
+    condicion.instance_variable_set(:@mensaje, mensaje)
+    condicion
+  end
+
   def verificar(objeto)
-    @proposicion.call(objeto)
+    raise TadspecException.new(@mensaje.call(objeto)) unless @proposicion.call(objeto)
   end
 end
