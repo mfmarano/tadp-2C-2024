2def75fe82d5c33bd8ccda58a8056b832ed411b8
mfmarano <mmarano@frba.utn.edu.ar>
2024-11-12 21:10:13 -0300

    refactor: definir flatMap, simplificar combinators

--- scala/src/main/scala/Parser.scala
+++ scala/src/main/scala/Parser.scala
@@ -5,26 +5,25 @@ case class Parser[+T](parse: String => Try[(T, String)]) {
     parse(input).recoverWith(_ => otherParser.parse(input))
   }
 
-  def <>[A](otherParser: => Parser[A]): Parser[(T, A)] = Parser { input =>
-    for {
-      (result1, rest1) <- parse(input)
-      (result2, rest2) <- otherParser.parse(rest1)
-    } yield ((result1, result2), rest2)
-  }
+  def <>[A](otherParser: => Parser[A]): Parser[(T, A)] = for {
+    thisResult <- this
+    otherResult <- otherParser
+  } yield (thisResult, otherResult)
 
-  def ~>[A](otherParser: => Parser[A]): Parser[A] = Parser { input =>
-    (this <> otherParser).parse(input)
-      .map { case ((_, result), rest) => (result, rest) }
-  }
+  def ~>[A](otherParser: => Parser[A]): Parser[A] = for {
+    _ <- this
+    otherResult <- otherParser
+  } yield otherResult
 
-  def <~[A](otherParser: => Parser[A]): Parser[T] = Parser { input =>
-    (this <> otherParser).parse(input)
-      .map { case ((result, _), rest) => (result, rest) }
-  }
+  def <~[A](otherParser: => Parser[A]): Parser[T] = for {
+    thisResult <- this
+    _ <- otherParser
+  } yield thisResult
 
-  def sepBy[A](sep: => Parser[A]): Parser[List[T]] =
-    (this <> (sep ~> this).*)
-      .map { (result, rest) => result :: rest }
+  def sepBy[A](sep: => Parser[A]): Parser[List[T]] = for {
+    thisResult <- this
+    restSeparated <- (sep ~> this).*
+  } yield thisResult :: restSeparated
 
   def satisfies(condition: T => Boolean): Parser[T] = Parser { input =>
     parse(input).flatMap { (result, rest) =>
@@ -41,21 +40,20 @@ case class Parser[+T](parse: String => Try[(T, String)]) {
   }
 
   def * : Parser[List[T]] = Parser { input =>
-    +.parse(input).recover {
-      _ => (List.empty[T], input)
-    }
+    +.parse(input).recover { _ => (List.empty[T], input) }
   }
 
-  def + : Parser[List[T]] = Parser { input =>
-    parse(input).flatMap { (result, rest) =>
-      *.parse(rest).map { (moreResults, finalRest) =>
-        (result :: moreResults, finalRest)
-      }
-    }
-  }
+  def + : Parser[List[T]] = for {
+    thisResult <- this
+    rest <- this.*
+  } yield thisResult :: rest
 
   def map[A](f: T => A): Parser[A] = Parser { input =>
     parse(input).map { (result, rest) => (f(result), rest) }
+  }
+
+  def flatMap[A](f: T => Parser[A]): Parser[A] = Parser { input =>
+    parse(input).flatMap { (result, rest) => f(result).parse(rest) }
   }
 }
 
@@ -80,14 +78,14 @@ object Parsers {
   val integer: Parser[Int] = Parser { input =>
     val negativeDigits = char('-').opt <> digit.*
     negativeDigits.parse(input).flatMap((result, rest) => result match {
-      case (Some(menos), list) => Success((menos::list).mkString.toInt, rest)
+      case (Some(menos), list) => Success((menos :: list).mkString.toInt, rest)
       case (None, list) => Success(list.mkString.toInt, rest)
     })
   }
 
   val double: Parser[Double] = Parser { input =>
     val decimalParser = integer <> char('.').opt <> digit.*
-    decimalParser.parse(input).flatMap( (result, rest) => result match {
+    decimalParser.parse(input).flatMap((result, rest) => result match {
       case ((int, Some(_)), list) => Success((int.toString + "." + list.mkString).toDouble, rest)
       case ((int, None), _) => Success(int.toDouble, rest)
     })
